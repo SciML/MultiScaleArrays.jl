46db0fed643457ab16b150421d25022ac2a1dc2f
ChrisRackauckas <chrisrackauckas@gmail.com>
2017-05-03 15:45:59 -0700

    broadcast v0.6

--- src/math.jl
+++ src/math.jl
@@ -83,6 +83,25 @@ if VERSION < v"0.6-"
 
 end
 
+add_idxs(x,expr) = expr
+add_idxs{T<:AbstractMultiScaleArray}(::Type{T},expr) = :($(expr).x[i])
+
+add_y(x,expr) = expr
+add_y{T<:AbstractMultiScaleArray}(::Type{T},expr) = :($(expr).y)
+
+@generated function Base.broadcast!(f,m::AbstractMultiScaleArray,B::Union{Number,AbstractMultiScaleArray}...)
+  exs = ((add_idxs(B[i],:(B[$i])) for i in eachindex(B))...)
+  exs_y = ((add_y(B[i],:(B[$i])) for i in eachindex(B))...)
+  quote
+    for i in eachindex(A.x)
+      broadcast!(f,A.x[i],$(exs...))
+    end
+    if !(typeof(m)<:AbstractMultiScaleArrayLeaf) && !isempty(y)
+      broadcast!(f,A.y,$(exs_y...))
+    end
+  end
+end
+
 
 *(m::AbstractMultiScaleArray,y::Number) = m.*y
 *(y::Number,m::AbstractMultiScaleArray) = m.*y
