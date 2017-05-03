d89bd97bb834fa7f2f2fcbd491deecfec0ba991d
ChrisRackauckas <chrisrackauckas@gmail.com>
2017-05-03 14:35:00 -0700

    broadcast overloads only on v0.5

--- src/math.jl
+++ src/math.jl
@@ -1,96 +1,97 @@
 ### Necessary until broadcast changes
 
-function .*(m::AbstractMultiScaleArray,y::Number)
-  new_m = similar(m)
-  for i in eachindex(m)
-    new_m[i] = m[i]*y
+if VERSION < v"0.6-"
+  function .*(m::AbstractMultiScaleArray,y::Number)
+    new_m = similar(m)
+    for i in eachindex(m)
+      new_m[i] = m[i]*y
+    end
+    new_m
   end
-  new_m
+
+  function .*(m::AbstractMultiScaleArray,m2::AbstractMultiScaleArray)
+    new_m = similar(m)
+    for i in eachindex(m)
+      new_m[i] = m[i]*m2[i]
+    end
+    new_m
+  end
+
+  function .+(m::AbstractMultiScaleArray,y::Number)
+    new_m = similar(m)
+    for i in eachindex(m)
+      new_m[i] = m[i]+y
+    end
+    new_m
+  end
+
+  function .+(m::AbstractMultiScaleArray,m2::AbstractMultiScaleArray)
+    new_m = similar(m)
+    for i in eachindex(m)
+      new_m[i] = m[i]+m2[i]
+    end
+    new_m
+  end
+
+  function ./(m::AbstractMultiScaleArray,y::Number)
+    new_m = similar(m)
+    for i in eachindex(m)
+      new_m[i] = m[i]/y
+    end
+    new_m
+  end
+
+  function ./(y::Number,m::AbstractMultiScaleArray)
+    new_m = similar(m)
+    for i in eachindex(m)
+      new_m[i] = y/m[i]
+    end
+    new_m
+  end
+
+  function ./(m::AbstractMultiScaleArray,m2::AbstractMultiScaleArray)
+    new_m = similar(m)
+    for i in eachindex(m)
+      new_m[i] = m[i]/m2[i]
+    end
+    new_m
+  end
+
+  function .-(m::AbstractMultiScaleArray,y::Number)
+    new_m = similar(m)
+    for i in eachindex(m)
+      new_m[i] = m[i]-y
+    end
+    new_m
+  end
+
+  function .-(y::Number,m::AbstractMultiScaleArray)
+    new_m = similar(m)
+    for i in eachindex(m)
+      new_m[i] = y-m[i]
+    end
+    new_m
+  end
+
+  function .-(m::AbstractMultiScaleArray,m2::AbstractMultiScaleArray)
+    new_m = similar(m)
+    for i in eachindex(m)
+      new_m[i] = m[i]-m2[i]
+    end
+    new_m
+  end
+
 end
 
-function .*(m::AbstractMultiScaleArray,m2::AbstractMultiScaleArray)
-  new_m = similar(m)
-  for i in eachindex(m)
-    new_m[i] = m[i]*m2[i]
-  end
-  new_m
-end
 
 *(m::AbstractMultiScaleArray,y::Number) = m.*y
 *(y::Number,m::AbstractMultiScaleArray) = m.*y
-
-function .+(m::AbstractMultiScaleArray,y::Number)
-  new_m = similar(m)
-  for i in eachindex(m)
-    new_m[i] = m[i]+y
-  end
-  new_m
-end
-
-function .+(m::AbstractMultiScaleArray,m2::AbstractMultiScaleArray)
-  new_m = similar(m)
-  for i in eachindex(m)
-    new_m[i] = m[i]+m2[i]
-  end
-  new_m
-end
-
 +(m::AbstractMultiScaleArray,y::AbstractMultiScaleArray) = m.+y
 +(m::AbstractMultiScaleArray,y::Number) = m.+y
 +(y::Number,m::AbstractMultiScaleArray) = m.+y
 
-function ./(m::AbstractMultiScaleArray,y::Number)
-  new_m = similar(m)
-  for i in eachindex(m)
-    new_m[i] = m[i]/y
-  end
-  new_m
-end
-
-function ./(y::Number,m::AbstractMultiScaleArray)
-  new_m = similar(m)
-  for i in eachindex(m)
-    new_m[i] = y/m[i]
-  end
-  new_m
-end
-
-function ./(m::AbstractMultiScaleArray,m2::AbstractMultiScaleArray)
-  new_m = similar(m)
-  for i in eachindex(m)
-    new_m[i] = m[i]/m2[i]
-  end
-  new_m
-end
-
 /(m::AbstractMultiScaleArray,y::Number) = m./y
 /(y::Number,m::AbstractMultiScaleArray) = y./m
-
-
-
-function .-(m::AbstractMultiScaleArray,y::Number)
-  new_m = similar(m)
-  for i in eachindex(m)
-    new_m[i] = m[i]-y
-  end
-  new_m
-end
-
-function .-(y::Number,m::AbstractMultiScaleArray)
-  new_m = similar(m)
-  for i in eachindex(m)
-    new_m[i] = y-m[i]
-  end
-  new_m
-end
-
-function .-(m::AbstractMultiScaleArray,m2::AbstractMultiScaleArray)
-  new_m = similar(m)
-  for i in eachindex(m)
-    new_m[i] = m[i]-m2[i]
-  end
-  new_m
-end
 
 -(m::AbstractMultiScaleArray,y::AbstractMultiScaleArray) = m.-y
 -(m::AbstractMultiScaleArray,y::Number) = m.-y
