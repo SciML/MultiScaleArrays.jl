b7d176e4934e8eb9926ac4ee452bc1f32d76509f
ChrisRackauckas <chrisrackauckas@gmail.com>
2017-05-03 15:51:52 -0700

    comment out broadcasting for later

--- src/math.jl
+++ src/math.jl
@@ -81,27 +81,28 @@ if VERSION < v"0.6-"
     new_m
   end
 
-end
+else # Only define broadcasts on v0.6+
+  #=
+  add_idxs(x,expr) = expr
+  add_idxs{T<:AbstractMultiScaleArray}(::Type{T},expr) = :($(expr).x[i])
 
-add_idxs(x,expr) = expr
-add_idxs{T<:AbstractMultiScaleArray}(::Type{T},expr) = :($(expr).x[i])
+  add_y(x,expr) = expr
+  add_y{T<:AbstractMultiScaleArray}(::Type{T},expr) = :($(expr).y)
 
-add_y(x,expr) = expr
-add_y{T<:AbstractMultiScaleArray}(::Type{T},expr) = :($(expr).y)
-
-@generated function Base.broadcast!(f,A::AbstractMultiScaleArray,B::Union{Number,AbstractMultiScaleArray}...)
-  exs = ((add_idxs(B[i],:(B[$i])) for i in eachindex(B))...)
-  exs_y = ((add_y(B[i],:(B[$i])) for i in eachindex(B))...)
-  quote
-    for i in eachindex(A.x)
-      broadcast!(f,A.x[i],$(exs...))
-    end
-    if !(typeof(A)<:AbstractMultiScaleArrayLeaf) && !isempty(y)
-      broadcast!(f,A.y,$(exs_y...))
+  @generated function Base.broadcast!(f,A::AbstractMultiScaleArray,B::Union{Number,AbstractMultiScaleArray}...)
+    exs = ((add_idxs(B[i],:(B[$i])) for i in eachindex(B))...)
+    exs_y = ((add_y(B[i],:(B[$i])) for i in eachindex(B))...)
+    quote
+      for i in eachindex(A.x)
+        broadcast!(f,A.x[i],$(exs...))
+      end
+      if !(typeof(A)<:AbstractMultiScaleArrayLeaf) && !isempty(y)
+        broadcast!(f,A.y,$(exs_y...))
+      end
     end
   end
+  =#
 end
-
 
 *(m::AbstractMultiScaleArray,y::Number) = m.*y
 *(y::Number,m::AbstractMultiScaleArray) = m.*y
