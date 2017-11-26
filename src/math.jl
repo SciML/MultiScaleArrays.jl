1a7e8d3ff46ed99b7fa82950d0363dd1f1fe73ea
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2017-11-26 03:27:52 -0800

    Fix dynamic_diffeq tests

--- src/math.jl
+++ src/math.jl
@@ -11,6 +11,8 @@ Base.Broadcast.promote_containertype(::Type{T}, ::Type{S}) where {T<:AMSA, S<:Ab
 Base.Broadcast.promote_containertype(::Type{S}, ::Type{T}) where {T<:AMSA, S<:AbstractArray} = T
 Base.Broadcast.promote_containertype(::Type{T}, ::Type{<:Any}) where {T<:AMSA} = T
 Base.Broadcast.promote_containertype(::Type{<:Any}, ::Type{T}) where {T<:AMSA} = T
+Base.Broadcast.promote_containertype(::Type{Array}, ::Type{T}) where {T<:AMSA} = T
+Base.Broadcast.promote_containertype(::Type{T}, ::Type{Array}) where {T<:AMSA} = T
 Base.Broadcast._containertype(::Type{T}) where {T<:AMSA} = T
 Base.Broadcast.broadcast_indices(::Type{<:AMSA}, A) = indices(A)
 
