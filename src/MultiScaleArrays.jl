bec481f67eb8c4ccb5e4b2da67b68616f143980b
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-07-18 18:07:22 -0700

    fix some depwarns

--- src/MultiScaleArrays.jl
+++ src/MultiScaleArrays.jl
@@ -14,7 +14,7 @@ abstract type AbstractMultiScaleArray{B}     <: AbstractVector{B} end
 abstract type AbstractMultiScaleArrayLeaf{B} <: AbstractMultiScaleArray{B} end
 abstract type AbstractMultiScaleArrayHead{B} <: AbstractMultiScaleArray{B} end
 
-using DiffEqBase
+using DiffEqBase, Statistics
 
 Base.show(io::IO, x::AbstractMultiScaleArray) = invoke(show, Tuple{IO, Any}, io, x)
 Base.show(io::IO, ::MIME"text/plain", x::AbstractMultiScaleArray) = show(io, x)
