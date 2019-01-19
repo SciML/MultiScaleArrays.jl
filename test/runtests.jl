62e753b4a151dc6b5a2408e25cf08934091727fa
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2019-01-19 05:14:43 -0500

    uncomment tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,11 @@
 using MultiScaleArrays, OrdinaryDiffEq, DiffEqBase, StochasticDiffEq
 using Test
 
-#@time @testset "Tuple Nodes" begin include("tuple_nodes.jl") end
+@time @testset "Tuple Nodes" begin include("tuple_nodes.jl") end
 @time @testset "Bisect Search Tests" begin include("bisect_search_tests.jl") end
 @time @testset "Indexing and Creation Tests" begin include("indexing_and_creation_tests.jl") end
 @time @testset "Values Indexing" begin include("values_indexing.jl") end
 @time @testset "Get Indices Tests" begin include("get_indices.jl") end
 @time @testset "Additional Fields Test" begin include("additional_fields_test.jl") end
 @time @testset "Dynamic DiffEq Tests" begin include("dynamic_diffeq.jl") end
-#@time @testset "Single Layer DiffEq Tests" begin include("single_layer_diffeq.jl") end
+@time @testset "Single Layer DiffEq Tests" begin include("single_layer_diffeq.jl") end
